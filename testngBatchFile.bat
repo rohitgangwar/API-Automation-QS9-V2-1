367ace04f5a427c9a183024fc5f20278e02fabc4
KaushikRohan <rohan.kaushik@sap.com>
2019-03-20 05:50:26 +0530

    Delete testngBatchFile.bat

--- testngBatchFile.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\EPPMAPIAutomation\EppmAPIFramework
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng1.xml
-pause
