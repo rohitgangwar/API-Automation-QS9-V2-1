ea0c3fb9a6f0ac4743e98ff3d63ac532a261f616
KaushikRohan <rohan.kaushik@sap.com>
2020-07-05 17:10:13 +0530

    Update ServiceUrl.java
    Modifying to V2 API url

--- src/main/java/eppmAPIFramework/com/rest/service/ServiceUrl.java
+++ src/main/java/eppmAPIFramework/com/rest/service/ServiceUrl.java
@@ -48,21 +48,21 @@ public class ServiceUrl {
 	 * 
 	 */
 
-	public final static String createProjectUrl = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProject";
-	public final static String GET_SYSTEM_DETAILS = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProject";
-	public final static String GET_SYSTEM_DETAILS1 = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProjectElement?$filter=ProjectElement eq 'TEST_QW12345_PJT1_1'";
-	public final static String CREATE_WBS_ELEMENTS_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/SAP/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProject(guid'{GUID}')/to_EnterpriseProjectElement";
-	public final static String GET_PROJECTDETAILS_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProject?$filter=Project eq '{pName}'";
-	public final static String UPDATE_WBS_ELEMENT_ATTRIBUTES_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProjectElement(guid'{GUID}')";
-	public final static String DELETE_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProject(guid'{GUID}')";
-	public final static String RELEASE_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/SetProcessingStatus?ProjectUUID=guid'{GUID}'&ProcessingStatus='10'";
-	public final static String TCHO_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/SetProcessingStatus?ProjectUUID=guid'{GUID}'&ProcessingStatus='40'";
-	public final static String LOCKED_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/SetProcessingStatus?ProjectUUID=guid'{GUID}'&ProcessingStatus='20'";
-	public final static String CLOSED_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/SetProcessingStatus?ProjectUUID=guid'{GUID}'&ProcessingStatus='42'";
-	public final static String createBlockFunctionPSTUrl = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProjBlkFunc";
-	public final static String UPDATE_PST_BLOCK_FUNCTION_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProjBlkFunc(guid'{GUID}')";
-	public final static String GET_PST_BLOCK_FUNCTIONS_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EnterpriseProjBlkFunc(guid'{pUUID}')";
-	public final static String Create_Block_Function_WBS_URl = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV/A_EntProjElmntBlockFunc";
+	public final static String createProjectUrl = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProject";
+	public final static String GET_SYSTEM_DETAILS = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProject";
+	public final static String GET_SYSTEM_DETAILS1 = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProjectElement?$filter=ProjectElement eq 'TEST_QW12345_PJT1_1'";
+	public final static String CREATE_WBS_ELEMENTS_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/SAP/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProject(guid'{GUID}')/to_EnterpriseProjectElement";
+	public final static String GET_PROJECTDETAILS_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProject?$filter=Project eq '{pName}'";
+	public final static String UPDATE_WBS_ELEMENT_ATTRIBUTES_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProjectElement(guid'{GUID}')";
+	public final static String DELETE_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProject(guid'{GUID}')";
+	public final static String RELEASE_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/SetProcessingStatus?ProjectUUID=guid'{GUID}'&ProcessingStatus='10'";
+	public final static String TCHO_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/SetProcessingStatus?ProjectUUID=guid'{GUID}'&ProcessingStatus='40'";
+	public final static String LOCKED_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/SetProcessingStatus?ProjectUUID=guid'{GUID}'&ProcessingStatus='20'";
+	public final static String CLOSED_PROJECT_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/SetProcessingStatus?ProjectUUID=guid'{GUID}'&ProcessingStatus='42'";
+	public final static String createBlockFunctionPSTUrl = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProjBlkFunc";
+	public final static String UPDATE_PST_BLOCK_FUNCTION_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProjBlkFunc(guid'{GUID}')";
+	public final static String GET_PST_BLOCK_FUNCTIONS_URL = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EnterpriseProjBlkFunc(guid'{pUUID}')";
+	public final static String Create_Block_Function_WBS_URl = "https://qs9-711-api.wdf.sap.corp/sap/opu/odata/sap/API_ENTERPRISE_PROJECT_SRV;v=0002/A_EntProjElmntBlockFunc";
 
 
 
