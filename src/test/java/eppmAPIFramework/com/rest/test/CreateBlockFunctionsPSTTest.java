99a93121ff2d0aefaaa9c8939fbd64b011017a60
KaushikRohan <rohan.kaushik@sap.com>
2020-07-08 23:22:47 +0530

    Update CreateBlockFunctionsPSTTest.java
    Adding block functions scenarios for project

--- src/test/java/eppmAPIFramework/com/rest/test/CreateBlockFunctionsPSTTest.java
+++ src/test/java/eppmAPIFramework/com/rest/test/CreateBlockFunctionsPSTTest.java
@@ -14,49 +14,49 @@ public class CreateBlockFunctionsPSTTest {
 	
 	Service service = new Service();
 
-//	/**
-//	 * Assert to Validate Creation of Single Block Functions
-//	 * for Overhead project
-//	 * 
-//	 */
-//	@Test(priority = 1)
-//	public void canCreateOVHBlockFunctionPST() {
-//		Assert.assertTrue(service.verifyCreateSingleBFPSTOVHProj(),
-//				"Create Block Function API Failed for Overhead Project");
-//	}
-//	
-//	/**
-//	 * Assert to Validate Creation of multiple Block Functions
-//	 * for Investment project
-//	 * 
-//	 */
-//	@Test(priority = 2)
-//	public void canCreateINVBlockFunctionPST() {
-//		Assert.assertTrue(service.verifyCreateMultipleBFPSTINVProj(),
-//				"Create Block Function API Failed for Investment Project");
-//	}
-//	
-//	/**
-//	 * Assert to Validate Creation of multiple Block Functions
-//	 * for Statistical project
-//	 * 
-//	 */
-//	@Test(priority = 3)
-//	public void canCreateStatProjBFPST() {
-//		Assert.assertTrue(service.verifyCreateMultipleBFPSTSTATProj(),
-//				"Create Block Function API Failed for Statistical Project");
-//	}
-//	
-//	/**
-//	 * Assert to Validate Creation of multiple Block Functions
-//	 * for Revenue project
-//	 * 
-//	 */
-//	@Test(priority = 4)
-//	public void canCreateRevProjBFPST() {
-//		Assert.assertTrue(service.verifyCreateMultipleBFPSTRevProj(),
-//				"Create Block Function API Failed for Revenue Project");
-//	}
+	/**
+	 * Assert to Validate Creation of Single Block Functions
+	 * for Overhead project
+	 * 
+	 */
+	@Test(priority = 1)
+	public void canCreateOVHBlockFunctionPST() {
+		Assert.assertTrue(service.verifyCreateSingleBFPSTOVHProj(),
+				"Create Block Function API Failed for Overhead Project");
+	}
+	
+	/**
+	 * Assert to Validate Creation of multiple Block Functions
+	 * for Investment project
+	 * 
+	 */
+	@Test(priority = 2)
+	public void canCreateINVBlockFunctionPST() {
+		Assert.assertTrue(service.verifyCreateMultipleBFPSTINVProj(),
+				"Create Block Function API Failed for Investment Project");
+	}
+	
+	/**
+	 * Assert to Validate Creation of multiple Block Functions
+	 * for Statistical project
+	 * 
+	 */
+	@Test(priority = 3)
+	public void canCreateStatProjBFPST() {
+		Assert.assertTrue(service.verifyCreateMultipleBFPSTSTATProj(),
+				"Create Block Function API Failed for Statistical Project");
+	}
+	
+	/**
+	 * Assert to Validate Creation of multiple Block Functions
+	 * for Revenue project
+	 * 
+	 */
+	@Test(priority = 4)
+	public void canCreateRevProjBFPST() {
+		Assert.assertTrue(service.verifyCreateMultipleBFPSTRevProj(),
+				"Create Block Function API Failed for Revenue Project");
+	}
 	
 	/**
 	 * Assert to Validate Creation of multiple Block Functions
